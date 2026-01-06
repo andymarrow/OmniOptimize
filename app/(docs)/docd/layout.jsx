4c09989a3f280c70bd9d31d9a0ee074c1bc70556
dagota12 <chernetdagim@gmail.com>
2026-01-06 12:02:06 +0300

    feat:  initialize sdk docs and restructure files to support docs - fixed css conflict b/n nextra and tailwind

--- app/(docs)/docd/layout.jsx
+++ app/(docs)/docd/layout.jsx
@@ -1,8 +1,7 @@
-
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
 import { Banner, Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
-
+import "nextra-theme-docs/style-prefixed.css";
 const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
 
 const navbar = <Navbar logo={<b>Omni SDK</b>} />;
@@ -11,16 +10,19 @@ const footer = <Footer>© {new Date().getFullYear()} Omni</Footer>;
 
 export default async function DocsLayout({ children }) {
   return (
-    <div className="nextra-theme-docs">
-      <Layout
-        banner={banner}
-        navbar={navbar}
-        pageMap={await getPageMap('/docd')}
-        footer={footer}
-      >
-        {children}
-      </Layout>
-    </div>
+    <html lang="en">
+      <body>
+        <div className="nextra-theme-docs">
+          <Layout
+            banner={banner}
+            navbar={navbar}
+            pageMap={await getPageMap("/docd")}
+            footer={footer}
+          >
+            {children}
+          </Layout>
+        </div>
+      </body>
+    </html>
   );
 }
-
