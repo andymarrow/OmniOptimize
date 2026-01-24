eb75beef6f375844a71e628f5b6ad4c9ea1a60c5
wokuma <wokumateshome13@gmail.com>
2026-01-24 14:56:40 +0300

    Update ml_service API

--- app/ml_service/api.py
+++ app/ml_service/api.py
@@ -1,4 +1,4 @@
-# ml_service/api.py
+# import the frameworks and library
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -26,6 +26,7 @@ try:
 except Exception as e:
     print(f"❌ Error loading model: {e}")
 
+# feature trained in machine
 class SeoData(BaseModel):
     performance_score: float
     accessibility_score: float
